6d02ecddb4603fe54b3b1de03e24d31cb99ca278
rc-regalado <ruben.alekey@gmail.com>
2020-10-21 15:23:23 -0600

    Removed MaterialAdapter

--- src/main.js
+++ src/main.js
@@ -2,18 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-import VueMaterial from 'vue-material-adapter'
 import { VueSvgIcon } from '@yzfe/vue-svgicon'
-import CompositionApi from '@vue/composition-api'
 import { VTooltip } from 'v-tooltip'
 import '@yzfe/svgicon/lib/svgicon.css'
 
-Vue.use(CompositionApi)
-// Vue.use(VTooltip)
 Vue.directive('tooltip', VTooltip)
 Vue.config.productionTip = false
-Vue.use(VueMaterial)
+
 Vue.component('icon', VueSvgIcon)
 
 const vm = new Vue({
